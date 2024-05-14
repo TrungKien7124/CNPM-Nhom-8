feb6901745a805fe4c694a4fe5e52d750da7ca6f
TrungKien <trungkien7124@gmail.com>
2024-05-14 20:00:37 +0700

    them tinh nang dang nhap dang ky

--- Calendy/src/public/js/login.js
+++ Calendy/src/public/js/login.js
@@ -1,6 +1,7 @@
 let loginButton = document.querySelector(".loginButton");
 
 const loginAPI = 'http://localhost:3000/api/login';
+const logoutAPI = 'http://localhost:3000/api/logout';
 
 loginButton.addEventListener("click", () => {
     fetch(loginAPI, {
@@ -10,7 +11,11 @@ loginButton.addEventListener("click", () => {
         }
     })
     .then((response) => {
+
+        // Thong bao ra man hinh
         alert('Login successfully!');
+
+        // Chuyen huong sang home page
         window.location.href = '/';
         return response.json();
     }) 
